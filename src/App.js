5276aec056844ab1fd47168c195be22de8a5b1d1
trungttr <trinhtrung15052002@gmail.com>
2023-05-17 10:45:39 +0700

    New Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { Home } from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Booking } from './pages/Booking';
-import Cal from './components/Header/calender.js';
+import Calender from './components/Header/calender';
 export default function App() {
   return (
     <Router>
       <div className='App'>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Booking" element={<Booking />} />
